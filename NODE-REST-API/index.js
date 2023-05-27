ede8d7df0d7fbfb50d71aa5025f3f43141aaf6e8
Kiran Rawal <rawalkiran19@gmail.com>
2023-05-27 18:31:50 +0545

    Added RESTApi

---
+++ NODE-REST-API/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app= express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL)
+.then(console.log("Connected to mongodb"))
+.catch((err)=>console.log(err));
+
+//middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use("/api/users",userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts",postRoute);
+
+
+
+app.listen(8800,()=>{
+    console.log("backend server is runnning...");
+})
